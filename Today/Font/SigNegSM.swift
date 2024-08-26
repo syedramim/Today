823fe98a38707bd07a5cf796bfe92e237fd50683
Kaneis Zontanos <ramim@bc.edu>
2024-08-26 12:30:32 -0400

    Initial Commit

---
+++ Today/Font/SigNegSM.swift
@@ -0,0 +1,18 @@
+//
+//  SigNegSM.swift
+//  Today
+//
+//  Created by Kaneis Zontanos on 8/26/24.
+//
+
+import SwiftUI
+
+struct SigNegSM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SigNegSM()
+}
---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Kaneis Zontanos on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
